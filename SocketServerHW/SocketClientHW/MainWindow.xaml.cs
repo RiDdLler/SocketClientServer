ed2b0de4daca6c5f0e0d8802fd2c9aba6795a270
Kirill Savostyanov <109529075+RiDdLler@users.noreply.github.com>
2023-09-21 23:39:47 +0600

    Add files via upload

---
+++ SocketServerHW/SocketClientHW/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SocketClientHW
+{
+	public partial class MainWindow : Window
+	{
+		private TcpClient client;
+		private NetworkStream stream;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			client = new TcpClient();
+			ConnectToServer();
+		}
+
+		private async void ConnectToServer()
+		{
+			try
+			{
+				await client.ConnectAsync("127.0.0.1", 12345);
+				StatusTextBlock.Text = "Подключено к серверу";
+
+				stream = client.GetStream();
+
+				Task.Run(() => ReceiveMessages());
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Ошибка при подключении к серверу: " + ex.Message);
+			}
+		}
+
+		private async void SendButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				string request = RequestTextBox.Text;
+				byte[] buffer = Encoding.UTF8.GetBytes(request + "\n");
+
+				await stream.WriteAsync(buffer, 0, buffer.Length);
+
+				// Очистим поле ввода
+				RequestTextBox.Clear();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Ошибка при отправке запроса: " + ex.Message);
+			}
+		}
+
+		private async void ReceiveMessages()
+		{
+			byte[] buffer = new byte[1024];
+			string incompleteMessage = string.Empty;
+
+			while (true)
+			{
+				try
+				{
+					int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+					if (bytesRead <= 0)
+						break;
+
+					string response = incompleteMessage + Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+					string[] messages = response.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+					
+					if (messages.Length > 0)
+					{
+						incompleteMessage = messages[messages.Length - 1];
+					}
+
+
+					foreach (string message in messages)
+					{
+						Dispatcher.Invoke(() =>
+						{
+							ResponseTextBox.Text += "Получен ответ: " + message + "\n";
+						});
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Ошибка при получении сообщения: " + ex.Message);
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ SocketServerHW/SocketServerHW/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Server
+{
+	static async Task Main()
+	{
+		TcpListener listener = new TcpListener(IPAddress.Any, 12345);
+		listener.Start();
+		Console.WriteLine("Сервер запущен. Ожидание клиентов...");
+
+		while (true)
+		{
+			TcpClient client = await listener.AcceptTcpClientAsync();
+			Task.Run(() => HandleClient(client));
+		}
+	}
+
+	static async Task HandleClient(TcpClient client)
+	{
+		try
+		{
+			NetworkStream stream = client.GetStream();
+			byte[] buffer = new byte[1024];
+
+			while (true)
+			{
+				int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+				if (bytesRead <= 0)
+					break;
+
+				string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+				Console.WriteLine("Получен запрос: " + request);
+
+				// Здесь вы можете обработать запрос и отправить ответ
+				string response = "Ответ на запрос: " + request + "\n";
+				byte[] responseBuffer = Encoding.UTF8.GetBytes(response);
+				await stream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
+				Console.WriteLine("Отправлен ответ: " + response);
+			}
+
+			client.Close();
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("Ошибка при обработке клиента: " + ex.Message);
+		}
+	}
+}
